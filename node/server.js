11414ddc3dc0c8fb91b20eaf568de0d7bff5085b
praveen sinha <praveen@praveens-MacBook-Pro.local>
2017-01-20 20:45:09 -0800

    blah blah

--- node/server.js
+++ node/server.js
@@ -12,7 +12,7 @@ var app = express();
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
-    app.use(express.static(path.join(__dirname, '../')));
+    app.use(express.static(__dirname + '/../'));
 });
 
 console.log("app configured with " + __dirname);
