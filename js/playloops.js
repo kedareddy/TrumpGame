8275daadda059e021bc6f58f2f56abfb968c4a06
praveen sinha <dmhomee@gmail.com>
2017-02-09 17:40:20 -0800

    cleaned up file

--- js/playloops.js
+++ js/playloops.js
@@ -5,7 +5,7 @@
         return new Promise( function(resolve, reject) {
             const PLAYLOOPS_ADD_URL = PLAYLOOPS_SERVER_URL + "/playloops/";
         
-            alert(JSON.stringify(playloop_dict));
+            console.log(JSON.stringify(playloop_dict));
         
             $.ajax({
                 type: 'POST',
@@ -52,17 +52,7 @@
                 data: `file-name=${img_name}&file-type=${img_type}&content-encoding=base64`,
                 url: PLAYLOOPS_SIGN_URL,
             });
-        /*
-                success: function(signed_response){
-                        alert ("have a response");
-                        resolve(signed_response);
-                    },
-                error:  function(err) { 
-                    alert("Could not get signed signature for " + filename + " because " + err);
-                    reject("Could not get signed signature for " + filename + " because " + err);}
-            });
-            
-        });*/
+
     }
 
     function uploadFileToS3(file, contentType, signedRequest){
@@ -100,81 +90,6 @@
         });
     }
 
-
-    //img_name => foobar.gif
-    //img_type => MIME type like "image/gif" or "image/jpeg"
-    //img_src => base64 encoded image data
-
-    /*
-    function uploadImage(img_name, img_type, img_src, playloop_dict){
-        
-             var img_data = dataUri2Buffer( img_src );
-        
-             alert(img_data);
-             
-             const img_length = img_data.length;
-            
-             alert(img_length);
-        
-             $.ajax({
-                dataType: 'jsonp',
-                data: `file-name=${img_name}&file-type=${img_type}&content-encoding=base64`,
-                url: PLAYLOOPS_SIGN_URL,
-                success: 
-                 
-                    function (signed_response) {
-                        uploadFileToS3(img_data, 
-                                       img_type, 
-                                       signed_response.signedRequest, 
-                                       signed_response.url,
-                                       playloop_dict);
-                  
-                    },
-                 
-                error: 
-                 
-                    function(retdata) { 
-                        alert('Play loop could not be saved, Could not get signed URL :-()');
-                    }
-                 
-             });
-
-        }
-        */       
-
-/*        function uploadFileToS3(file, contentType, signedRequest, url, playloop_dict){
-            
-            playloop_dict['summary_img'] = url;
-            playloop_dict['playloop_url'] = PLAYLOOPS_SERVER_URL + '/' + playloop_dict['_id'];
-            
-            const xhr = new XMLHttpRequest();
-            sr = signedRequest;
-            
-            xhr.open('PUT', sr);
-            xhr.setRequestHeader('content-type', contentType);
-            xhr.setRequestHeader('content-encoding', 'base64');
-
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState === 4){
-                    if(xhr.status === 200){
-                        alert("uploaded file!  " + url);
-                        //document.getElementById('preview').src = url;
-                        
-                        //success_callback = function(data) { alert("playloop saved!"); }
-                        //error_callback = function(error) { alert("playloop save failed!!"); }
-                        
-                        //addPlayloop (playloop_dict, success_callback, error_callback);
-                    }         
-                        else{
-                            alert('Could not upload file.');
-                        }
-                    }
-                };
-                
-                xhr.send(file);
-        }
-  */      
-        
         
         function dataUri2Buffer(dataURI) {
             // from http://stackoverflow.com/questions/12883871/how-to-upload-base64-encoded-image-data-to-s3-using-javascript-only]
