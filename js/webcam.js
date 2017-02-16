801ac48426eea10b2e533ea8f0f0ec144bc74b24
kedar reddy <kkuddi89@gmail.com>
2017-02-15 17:43:35 -0800

    trying things

--- js/webcam.js
+++ js/webcam.js
@@ -149,15 +149,15 @@ var Webcam = {
 			var video = document.createElement('video');
 			video.setAttribute('autoplay', 'autoplay');
             video.id = "faceVideo";
-			video.style.width = '' + this.params.dest_width + 'px';
-			video.style.height = '' + this.params.dest_height + 'px';
-            /*video.style.width = "100%"; 
-            video.style.height = "auto";
+			//video.style.width = '' + this.params.dest_width + 'px';
+			//video.style.height = '' + this.params.dest_height + 'px';
+            video.style.width = "100%"; 
+            video.style.height = "100%";
             video.style.left = "0px";
             video.style.right = "0px";
             video.style.marginLeft = "auto";
             video.style.marginRight = "auto";
-            video.style.alignSelf = "center";*/
+            video.style.alignSelf = "center";
             
 			
 			if ((scaleX != 1.0) || (scaleY != 1.0)) {
