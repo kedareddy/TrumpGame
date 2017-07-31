22fdeab8f010521c97c5a56668097edd848a8423
Kedar Reddy <kkuddi89@gmail.com>
2017-07-30 17:39:24 -0700

    changing get to ajax call, and passing in playloop_dict

--- js/playloops.js
+++ js/playloops.js
@@ -35,13 +35,26 @@
     }
     
     //Kedar's additions
-    function createSummaryGIF(){
+    function createSummaryGIF(playloop_dict){
         const PLAYLOOPS_GET_URL = PLAYLOOPS_SERVER_URL + "/createSummaryGIF";
+        
+        $.ajax({
+            type: 'POST',
+            //dataType: 'jsonp',
+            data: playloop_dict,
+            url: PLAYLOOPS_GET_URL,
+            success : function(data) { resolve(playloop_dict) },
+            error : function(jqXHR, textStatus, errorThrown) {
+                console.log("Error: Status: "+textStatus+" Message: "+errorThrown);
+                reject("Error: Status:"+textStatus+" Message: "+errorThrown);
+            } 
+
+        });
     
-       $.get( PLAYLOOPS_GET_URL, function( data ) {
+       /*$.get( PLAYLOOPS_GET_URL, function( data ) {
            console.log( "##$#create Summary GIF result: " + data );
            //stitchGIF();
-        });
+        });*/
     }
 
     function stitchGIF(){
